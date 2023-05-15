8aaafaf3a1b9df838b3bbeca0edce73bea862ecf
EmanuelaPau <uapame@gmail.com>
2023-05-15 15:19:29 +0200

    Add ticket price calculation

--- script/my_script.js
+++ script/my_script.js
@@ -8,7 +8,16 @@
 
 
 // Add const age and kilometers
-const kilometers = document.getElementById("kilometers");
-console.log(kilometers);
-const age = document.getElementById("kilometers");
+// const kilometers = parseInt(document.getElementById("kilometers"));
+// console.log(kilometers);
+const age = parseInt(document.getElementById("age"));
 console.log(age);
+
+// Test WILL BE DELETED 
+kilometers = 1000;
+// age = 16;
+// age = 23; 
+// age = 91;
+// Calculate train ticket price 
+const ticketPrice = (kilometers - (kilometers * 0.233));
+console.log(ticketPrice);
