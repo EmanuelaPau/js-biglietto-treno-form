6182d97e2593beba546e59877dd5d710065f4ec6
EmanuelaPau <uapame@gmail.com>
2023-05-15 15:12:30 +0200

    Add kilometers and age const

---
+++ script/my_script.js
@@ -0,0 +1,14 @@
+// Scrivere un programma che chieda all’utente:
+// Il numero di chilometri da percorrere
+// Età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+// il prezzo del biglietto è definito in base ai km(0.233 € al km)
+// va applicato uno sconto del 19.4 % per i minorenni
+// va applicato uno sconto del 37.7 % per gli over 65.
+
+
+// Add const age and kilometers
+const kilometers = document.getElementById("kilometers");
+console.log(kilometers);
+const age = document.getElementById("kilometers");
+console.log(age);
